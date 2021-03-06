0a931c4dd7ae52c576657f53e08fe68539fd37ca
Oskar Eidem <oskareid@stud.ntnu.no>
2021-03-06 17:25:41 +0100

    Initial commit

---
+++ src/main/java/no/ntnu/prog2/hospital/Doctor.java
@@ -0,0 +1,4 @@
+package no.ntnu.prog2.hospital;
+
+public abstract class Doctor {
+}
---
+++ src/main/java/no/ntnu/prog2/hospital/Person.java
@@ -0,0 +1,46 @@
+package no.ntnu.prog2.hospital;
+
+public abstract class Person {
+    private String firstName;
+    private String lastName;
+    private String socialSecurityNumber;
+
+    public Person(String firstName, String lastName, String socialSecurityNumbers){};
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public String getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", socialSecurityNumber='" + socialSecurityNumber + '\'' +
+                '}';
+    }
+}
